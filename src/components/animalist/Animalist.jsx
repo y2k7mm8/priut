bacb1889369ff52b4ba7a9e6ef5dc89e09dc70a1
victorvladimirovich1 <jesin.tima.2006@gmail.com>
2025-04-24 10:50:04 +0600

    headerssss

--- src/components/animalist/Animalist.jsx
+++ src/components/animalist/Animalist.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import imh from "../../../src/assets/meow.mp3"
+import React from "react";
+import imh from "../../../src/assets/meow.mp3";
 export const Animalist = () => {
-  // Функция для проигрывания звука
   const playSound = (soundUrl) => {
-    const audio = new Audio(soundUrl); // создаем новый объект Audio
-    audio.play(); // воспроизводим звук
+    const audio = new Audio(soundUrl);
+    audio.play();
   };
 
   return (
     <>
-      <p className="flex text-5xl justify-center top-4 uppercase font-semibold">Наши мурчашие</p>
+      <p className="flex text-5xl justify-center top-4 uppercase font-semibold">
+        Наши мурчашие
+      </p>
       <div className="flex flex-col items-center mb-3 gap-6 border-2 border-gray-500 p-5 m-5 rounded-lg bg-gray-200">
         <div className="flex gap-4 font-semibold flex-nowrap">
           <div className="flex flex-col items-center">
@@ -17,7 +18,7 @@ export const Animalist = () => {
               className="w-72 h-96 object-contain"
               src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1yE00Sy00hgTdukr7JdwewYgCm_obp5dJn7A5jnpIDxwTg0f90eq610ABfXhQDaG0CE8&usqp=CAU"
               alt="Тигр"
-              onClick={() => playSound('../../../src/assets/meow.mp3')} // добавляем обработчик клика для звука
+              onClick={() => playSound("../../../src/assets/meow.mp3")}
             />
             <p className="mt-2 text-lg font-medium">Тигр</p>
           </div>
@@ -27,7 +28,7 @@ export const Animalist = () => {
               className="w-72 h-96 object-cover"
               src="https://idsb.tmgrup.com.tr/ly/uploads/images/2022/05/16/205719.jpg"
               alt="Ням Ням"
-              onClick={() => playSound('../../../src/assets/meow.mp3')} // добавляем обработчик клика для звука
+              onClick={() => playSound("../../../src/assets/meow.mp3")}
             />
             <p className="mt-2 text-lg">Ням Ням</p>
           </div>
@@ -37,7 +38,7 @@ export const Animalist = () => {
               className="w-72 h-96 object-cover"
               src="https://indoorpet.osu.edu/sites/default/files/assets/images/cats/Calico.jpg"
               alt="Нуртикек"
-              onClick={() => playSound('../../../src/assets/meow.mp3')} // добавляем обработчик клика для звука
+              onClick={() => playSound("../../../src/assets/meow.mp3")}
             />
             <p className="mt-2 text-lg">Нуртикек</p>
           </div>
@@ -47,7 +48,7 @@ export const Animalist = () => {
               className="w-72 h-96 object-cover"
               src="https://www.vetmed.wisc.edu/wp-content/uploads/2022/12/lina-angelov-Ah_QC2v2alE-unsplash-1200x960.jpg"
               alt="Сигмо"
-              onClick={() => playSound('p../../../src/assets/meow.mp3')} // добавляем обработчик клика для звука
+              onClick={() => playSound("p../../../src/assets/meow.mp3")}
             />
             <p className="mt-2 text-lg">Сигмо</p>
           </div>
@@ -57,7 +58,7 @@ export const Animalist = () => {
               className="w-72 h-96 object-cover"
               src="https://pettrust.uoguelph.ca/sites/default/files/iStock-472160419%20%281%29.jpg"
               alt="Милое имя"
-              onClick={() => playSound('')} // добавляем обработчик клика для звука
+              onClick={() => playSound("")}
             />
             <p className="mt-2 text-lg">Милое имя</p>
           </div>
@@ -67,7 +68,7 @@ export const Animalist = () => {
               className="w-72 h-96 object-cover"
               src="https://i.pinimg.com/736x/b4/bb/b2/b4bbb2198b036fe1024571ec6b60f8b8.jpg"
               alt="Кот"
-              onClick={() => playSound('../../../src/assets/meow.mp3')} // добавляем обработчик клика для звука
+              onClick={() => playSound("../../../src/assets/meow.mp3")}
             />
             <p className="mt-2 text-lg">Кот</p>
           </div>
@@ -77,7 +78,7 @@ export const Animalist = () => {
               className="w-72 h-96 object-cover"
               src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRy6snyVm8JuioMgHQZ1b15PR2fZovCc-WnvXm6bfuHc5kGPT6hJTxxY9EMVjYrodAKl8&usqp=CAU"
               alt="El Gato"
-              onClick={() => playSound('../../../src/assets/meow.mp3')} // добавляем обработчик клика для звука
+              onClick={() => playSound("../../../src/assets/meow.mp3")}
             />
             <p className="mt-2 text-lg">El gato</p>
           </div>
