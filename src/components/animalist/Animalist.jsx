c8d3808f78f3975ff4aedccfdad37c8c4359e91a
victorvladimirovich1 <jesin.tima.2006@gmail.com>
2025-04-26 09:31:19 +0600

    cattdsh

--- src/components/animalist/Animalist.jsx
+++ src/components/animalist/Animalist.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import imh from "../../../src/assets/meow.mp3"
+import React from "react";
+import imh from "../../assets/img/catt.png";
 export const Animalist = () => {
-
   const playSound = (soundUrl) => {
-    const audio = new Audio(soundUrl); 
-    audio.play(); 
+    const audio = new Audio(soundUrl);
+    audio.play();
   };
 
   return (
@@ -19,7 +18,7 @@ export const Animalist = () => {
               className="w-72 h-96 object-contain"
               src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1yE00Sy00hgTdukr7JdwewYgCm_obp5dJn7A5jnpIDxwTg0f90eq610ABfXhQDaG0CE8&usqp=CAU"
               alt="Тигр"
-              onClick={() => playSound('../../../src/assets/meow.mp3')} 
+              onClick={() => playSound("../../../src/assets/meow.mp3")}
             />
             <p className="mt-2 text-lg font-medium">Тигр</p>
           </div>
@@ -29,7 +28,7 @@ export const Animalist = () => {
               className="w-72 h-96 object-cover"
               src="https://idsb.tmgrup.com.tr/ly/uploads/images/2022/05/16/205719.jpg"
               alt="Ням Ням"
-              onClick={() => playSound('../../../src/assets/meow.mp3')} 
+              onClick={() => playSound("../../../src/assets/meow.mp3")}
             />
             <p className="mt-2 text-lg">Ням Ням</p>
           </div>
@@ -39,7 +38,7 @@ export const Animalist = () => {
               className="w-72 h-96 object-cover"
               src="https://indoorpet.osu.edu/sites/default/files/assets/images/cats/Calico.jpg"
               alt="Нуртикек"
-              onClick={() => playSound('../../../src/assets/meow.mp3')} 
+              onClick={() => playSound("../../../src/assets/meow.mp3")}
             />
             <p className="mt-2 text-lg">Нуртикек</p>
           </div>
@@ -49,7 +48,7 @@ export const Animalist = () => {
               className="w-72 h-96 object-cover"
               src="https://www.vetmed.wisc.edu/wp-content/uploads/2022/12/lina-angelov-Ah_QC2v2alE-unsplash-1200x960.jpg"
               alt="Сигмо"
-              onClick={() => playSound('p../../../src/assets/meow.mp3')} 
+              onClick={() => playSound("p../../../src/assets/meow.mp3")}
             />
             <p className="mt-2 text-lg">Сигмо</p>
           </div>
@@ -59,7 +58,7 @@ export const Animalist = () => {
               className="w-72 h-96 object-cover"
               src="https://pettrust.uoguelph.ca/sites/default/files/iStock-472160419%20%281%29.jpg"
               alt="Милое имя"
-              onClick={() => playSound('')} 
+              onClick={() => playSound("")}
             />
             <p className="mt-2 text-lg">Милое имя</p>
           </div>
@@ -69,7 +68,7 @@ export const Animalist = () => {
               className="w-72 h-96 object-cover"
               src="https://i.pinimg.com/736x/b4/bb/b2/b4bbb2198b036fe1024571ec6b60f8b8.jpg"
               alt="Кот"
-              onClick={() => playSound('../../../src/assets/meow.mp3')}
+              onClick={() => playSound("../../../src/assets/meow.mp3")}
             />
             <p className="mt-2 text-lg">Кот</p>
           </div>
@@ -79,7 +78,7 @@ export const Animalist = () => {
               className="w-72 h-96 object-cover"
               src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRy6snyVm8JuioMgHQZ1b15PR2fZovCc-WnvXm6bfuHc5kGPT6hJTxxY9EMVjYrodAKl8&usqp=CAU"
               alt="El Gato"
-              onClick={() => playSound('../../../src/assets/meow.mp3')} 
+              onClick={() => playSound("../../../src/assets/meow.mp3")}
             />
             <p className="mt-2 text-lg">El gato</p>
           </div>
@@ -89,7 +88,8 @@ export const Animalist = () => {
       <div className="flex justify-center mt-5 hover:scale-90 transition-transform duration-300">
         <button className="px-6 py-2 bg-blue-500 text-white font-semibold rounded-lg hover:border-l-cyan-800 shadow-md hover:bg-blue-600">
           Показать больше
-        </button> <img className="w-16 h-16 ml-4 object-cover" src=""/>
+        </button>{" "}
+        <img className="w-16 h-16 ml-4 object-cover" src={imh} />
       </div>
     </>
   );
